4f805f288cc79ca7ef652bbc77ce769ec23f7d14
thomasjoseph22 <116195232+thomasjoseph22@users.noreply.github.com>
2022-10-31 20:02:44 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,13 @@
+
+  /*=========================typing animation=========================*/
+
+  var typed = new Typed(".perso",{
+  
+    strings :["Thomas Joseph","Web Developer","Front End Developer","Ethical hacker"],
+    typespeed : 100,
+    backspeed : 80,
+    loop : true
+
+
+
+  })
